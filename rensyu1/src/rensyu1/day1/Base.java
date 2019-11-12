8563eccdd13b18003bacf6c9f2d6923ac6db2606
nucrab <nucrab22@gmail.com>
2019-11-12 15:00:42 +0900

    Qkbkbqqqq:q!
    icommit amend rkbtraialkbkbkbkbialýa:q!
    
    setkbkbkbiýakbsekbkbýasetkbkbkbkbskbkbkbýa::::kbkbkbkbsýaskbkbset enc_--kbkbkb+kb=utf-8ýaýaýaýaýaskbkbekbkbkbkbkbkbkbkbkbkbkbkbkbkbkbkbkbkbkbkbkbkbkbkbkbkbýa'ýaýa'se:set enc-\kbkb=utf-8:set tenc-kb=japan:set=tenckbkbkbkbkb tenc=korea:set tenc=japan]kbiýaýaýaýa+:wqkbkbq!
    
    :set enc=utf-8iýai한글þX 나옵니까?ýa:q!kbkbwq:xZZ:w firkbkbkbheresfilename:wq

---
+++ rensyu1/src/rensyu1/day1/Base.java
@@ -0,0 +1,9 @@
+package rensyu1.day1;
+
+public class Base {
+	String outputHello() {
+		String hello = "Hello";		
+		return hello;
+	}
+
+}
---
+++ rensyu1/src/rensyu1/day1/Korean.java
@@ -0,0 +1,9 @@
+package rensyu1.day1;
+
+public class Korean extends Base{
+	String outputHello() {
+		String hello = "안녕하세요!";
+		return hello;
+	}
+
+}
